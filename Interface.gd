05cc96e1f86089a0f091240a73d7cc971c2215fe
william <mrwm@wksp2>
2022-06-12 19:47:48 -0700

    add swipe detection to the app

--- Interface.gd
+++ Interface.gd
@@ -2,15 +2,21 @@ extends Control
 
 # Variables
 var timeValue = null;
-var requ = null;
 var showSeconds = false;
 var use24hour = false;
 
-onready var timeText = $"VBoxContainer/TimeLabel";
+onready var ampm = $"VBoxContainer/CenterContainer/TimeSplit/ampm";
+onready var hour = $"VBoxContainer/CenterContainer/TimeSplit/Hour";
+onready var minute = $"VBoxContainer/CenterContainer/TimeSplit/Minute";
+onready var second = $"VBoxContainer/CenterContainer/TimeSplit/Second";
+onready var touchpos = $"VBoxContainer/touchpos";
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
   #print(Engine.time_scale) #How fast we want the program to run
+  ampm.set_text("")
+  second.set_text("")
+  touchpos.set_text("") # leave empty for padding bottom of screen
   pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,9 +27,15 @@ func _process(_delta):
   var currentMinute = timeValue.minute;
   var currentSecond = timeValue.second;
 
-  # Toggle between 24 and 12 hour clocks
+  ampm.visible = false;
+  second.visible = false;
+
   if (currentHour > 12):
     isAM = false;
+    # Toggle between 24 and 12 hour clocks
+    if (!use24hour):
+      currentHour = currentHour - 12;
+      ampm.visible = true;
 
   # Pad numbers less than 10 with a leading zero
   if currentMinute < 10:
@@ -31,26 +43,68 @@ func _process(_delta):
   if currentSecond < 10:
     currentSecond = str("0") + str(currentSecond)
 
-  var formatedTime = " : " + str(currentMinute);
-
   if (showSeconds):
-    formatedTime = formatedTime + " : " + str(currentSecond)
+    second.visible = true;
+    second.set_text(": " + str(currentSecond))
   if (!use24hour):
-    currentHour = int(currentHour) - 12;
     if (isAM):
-      formatedTime = str(currentHour) + formatedTime + " a.m."
+      ampm.set_text("a.m.")
     else:
-      formatedTime = str(currentHour) + formatedTime + " p.m."
+      ampm.set_text("p.m.")
   else:
-    formatedTime = str(currentHour) + formatedTime
+    pass
 
-  timeText.set_text(formatedTime);
+  hour.set_text(str(currentHour));
+  minute.set_text(": " + str(currentMinute));
   pass
+
+# Figure out the swipe direction
+var initialPoint = Vector2(0,0);
+var secondPoint = Vector2(0,0);
+const vectorDeviation = Vector2(200, 500)
+func _input(event):
+  # The difference allowed for directional swipe
+
+  if (event is InputEventScreenTouch):
+    if (event.pressed):
+      initialPoint = event.position;
+
+  if (event is InputEventScreenTouch) and (!event.pressed):
+    secondPoint = event.position;
+    calculateDirection(initialPoint, secondPoint)
+
+func calculateDirection(firstPt, secondPt):
+  var deviation = Vector2(int(abs(firstPt.x - secondPt.x)), \
+                          int(abs(firstPt.y - secondPt.y)));
+
+  # Check which vector is dominant (X or Y)
+  if (deviation.x > deviation.y):
+  # Check direction of X
+    if (firstPt.x > secondPt.x) and (deviation.x > vectorDeviation.x):
+      #touchpos.set_text("initial larger x <-" + str(deviation.x))
+      pass
+    elif (firstPt.x < secondPt.x) and (deviation.x > vectorDeviation.x):
+      #touchpos.set_text("initial smaller x ->" + str(deviation.x))
+      pass
+    else:
+      #touchpos.set_text("no change in x: " + str(deviation.x))
+      pass
+  else:
+    # Check direction of Y
+    if (firstPt.y > secondPt.y) and (deviation.y > vectorDeviation.y):
+      #touchpos.set_text("initial larger y U " + str(deviation.y))
+      pass
+    elif (firstPt.y < secondPt.y) and (deviation.y > vectorDeviation.y):
+      #touchpos.set_text("initial smaller y D " + str(deviation.y))
+      pass
+    else:
+      #touchpos.set_text("no change in y: " + str(deviation.y))
+      pass
 
 func _on_CheckButton_toggled(button_pressed):
   showSeconds = button_pressed;
   pass
 
-func _on_CheckButton2_toggled(_button_pressed):
-  use24hour = !use24hour;
+func _on_CheckButton2_toggled(button_pressed):
+  use24hour = button_pressed;
   pass
