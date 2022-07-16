420d3e8419de731d413b47589b871785907d7ea3
william <wiiliamchung360@gmail.com>
2022-07-15 20:05:42 -0700

    attempt to fix lap deltas on stopwatch

--- Stopwatch.gd
+++ Stopwatch.gd
@@ -17,6 +17,8 @@ onready var lapContainer = $"LapContainer";
 
 var timer := Timer.new()
 
+var prevTime = null;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
   #Variables.currentScene = Variables.CurrentSceneIs.STOPWATCH
@@ -90,7 +92,47 @@ func _process(_delta):
     label.size_flags_horizontal = 3;
     label.size_flags_vertical = 1;
     # TODO: change lap to delta between each lap time instead of time lapsed
-    label.text = str(logged.hour) + ":" + str(logged.minute) + \
+    if lapContainer.get_child(0):
+      var currTime = {
+        hour = logged.hour,
+        minute = logged.minute,
+        second = logged.second,
+        milsec = logged.milsec,
+       }
+      var deltaTime = {
+        hour = int(currTime.hour) - int(prevTime.hour),
+        minute = int(currTime.minute) - int(prevTime.minute),
+        second = int(currTime.second) - int(prevTime.second),
+        milsec = int(currTime.milsec) - int(prevTime.milsec),
+       }
+      for key in deltaTime.keys():
+        if deltaTime[key] < 0:
+          var keyText = str(key)
+          if keyText == "milsec":
+            deltaTime[key] = int(currTime.milsec)*10 - int(prevTime.milsec)
+            deltaTime.second = int(currTime.second) - 1
+          if keyText == "second":
+            deltaTime[key] = int(currTime.milsec)*10 - int(prevTime.milsec)
+            deltaTime.minute = int(currTime.minute) - 1
+          if keyText == "minute":
+            deltaTime[key] = int(currTime.milsec)*10 - int(prevTime.milsec)
+            deltaTime.hour = int(currTime.hour) - 1
+      label.text = str(deltaTime.hour) + ":" + str(deltaTime.minute) + \
+                  ":" + str(deltaTime.second) + ":" + str(deltaTime.milsec)
+      prevTime = {
+        hour = logged.hour,
+        minute = logged.minute,
+        second = logged.second,
+        milsec = logged.milsec,
+       }
+    else:
+      prevTime = {
+        hour = logged.hour,
+        minute = logged.minute,
+        second = logged.second,
+        milsec = logged.milsec,
+       }
+      label.text = str(logged.hour) + ":" + str(logged.minute) + \
                   ":" + str(logged.second) + ":" + str(logged.milsec)
     label.name = "Lap-" + str(int(logged.milsec) + int(logged.second));
     lapContainer.add_child(label);
